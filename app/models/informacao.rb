9a6e8484b80d2c32bb7b19d75317646837da6725
bruno gonzaga <babsgonzaga@gmail.com>
2021-11-05 17:48:02 -0300

    Silo do Candidatos e pundit.

--- app/models/informacao.rb
+++ app/models/informacao.rb
@@ -2,4 +2,5 @@ class Informacao < ApplicationRecord
   belongs_to :candidato
 
   validates :titulo, :conteudo, :fonte, :data, :candidato_id, presence: true
+  # validates uniqueness combinado titulo e fonte?
 end
