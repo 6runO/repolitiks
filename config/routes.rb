837ca6dcfb2806fe6209008a5f7f1fad3616026d
bruno gonzaga <babsgonzaga@gmail.com>
2021-11-06 03:21:14 -0300

    Criado SILO básico do app, com autorizações e autenticações.

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,8 @@ Rails.application.routes.draw do
   root to: 'pages#home'
   get '/user' => "pages#minha", :as => :user_root
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  resources :candidatos
+  resources :candidatos, only: [:index, :show] do
+    resources :marcados, only: [:create]
+  end
+  resources :marcados, only: [:update]
 end
