a502a56d5a5236b386766835e69831e9c03ab329
Paulo Franco <paulohenriquefranco@gmail.com>
2021-11-08 14:22:20 -0300

    Busca básica

--- app/controllers/candidatos_controller.rb
+++ app/controllers/candidatos_controller.rb
@@ -4,6 +4,7 @@ class CandidatosController < ApplicationController
 
   def index
     @candidatos = policy_scope(Candidato)
+    @candidatos = Candidato.search(params[:search])
   end
 
   def show
@@ -14,5 +15,9 @@ class CandidatosController < ApplicationController
 
   def set_candidato
     @candidato = Candidato.find(params[:id])
+  end
+
+  def candidato_params
+    params.require(:candidato).permit(:nome_candidato, :candidato_id, :search)
   end
 end
--- app/models/candidato.rb
+++ app/models/candidato.rb
@@ -1,7 +1,22 @@
 class Candidato < ApplicationRecord
   has_many :marcados
   has_many :informacaos
+  belongs_to :politico
 
   validates :cpf, :nome_candidato, :email, :nascimento, :grau_instrucao, :ocupacao,
             :partido, :cargo, :estado, :nome_urna, :ano_eleicao, presence: true
+
+  def self.search(search)
+    if search
+      politico_buscado = Candidato.find_by(nome_candidato: search)
+      if politico_buscado
+        self.where(candidato_id: politico_buscado)
+      else
+        @candidatos = Candidato.all
+      end
+    else
+      @candidatos = Candidato.all
+    end
+  end
+
 end
