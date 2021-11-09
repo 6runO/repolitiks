a20fbcc8845bb83dad40a228a96c62bcd101990a
bruno gonzaga <babsgonzaga@gmail.com>
2021-11-09 15:04:56 -0300

    Pois é

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,13 +1,12 @@
 class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
   before_action :authenticate_user!
+  before_action :set_user_login
   include Pundit
 
   # Pundit: white-list approach.
   after_action :verify_authorized, except: :index, unless: :skip_pundit?
   after_action :verify_policy_scoped, only: :index, unless: :skip_pundit?
-
-  @user_login = User.new
 
   def after_sign_in_path_for(resource)
     stored_location_for(resource) || user_root_path
@@ -32,5 +31,9 @@ class ApplicationController < ActionController::Base
 
   def skip_pundit?
     devise_controller? || params[:controller] =~ /(^(rails_)?admin)|(^pages$)/
+  end
+
+  def set_user_login
+    @user_login = User.new
   end
 end
