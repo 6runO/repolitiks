e207fa6b2c82d13cf291e017a26bc80c961afeb3
Leonardo Rodrigues <leonardomyfiis@gmail.com>
2021-11-09 21:29:33 -0300

    Coloquei imagens em assets, fiz seed e inserir cl_image em show de candidatos

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,9 +7,12 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 
 require 'csv'
+puts "Apagando registros anteriores..."
+Candidato.delete_all
 
+puts "Criando novos registros..."
 csv_text = File.read(Rails.root.join('lib', 'seeds', 'candidatos_2018.csv'))
-csv = CSV.parse(csv_text, :headers => true, :encoding => 'ISO-8859-1')
+csv = CSV.parse(csv_text, :headers => true, :encoding => 'ISO-8859-2')
 csv.each do |row|
   c = Candidato.new
   c.cpf = row['CPF']
@@ -23,10 +26,11 @@ csv.each do |row|
   c.estado = row['ESTADO']
   c.nome_urna = row['NOME_URNA']
   c.ano_eleicao = row['ANO_ELEICAO']
+  c.status_eleicao = row['STATUS_ELEICAO']
 
   foto = row['SQ_CANDIDATO']
 
-  c.photo.attach(io: File.open("app/assets/images/#{foto}.jpg"), filename: "#{foto}.jpg")
+  c.photo.attach(io: File.open("app/assets/images/fotos_2018/#{foto}.jpg"), filename: "#{foto}.jpg")
 
   c.save
   puts "#{c.nome_urna}, #{c.partido} saved!"
