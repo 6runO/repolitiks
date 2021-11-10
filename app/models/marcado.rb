bbab2851c667ad9dde7319c0b86e6c5400f91808
bruno gonzaga <babsgonzaga@gmail.com>
2021-11-10 13:24:41 -0300

    Botão seguir codado e outras modificações.

--- app/models/marcado.rb
+++ app/models/marcado.rb
@@ -3,4 +3,6 @@ class Marcado < ApplicationRecord
   belongs_to :candidato
 
   validates :user_id, :candidato_id, presence: true
+  validates :user_id, uniqueness: { scope: :candidato_id,
+    message: "este candidato já foi marcado anteriormente" }
 end
