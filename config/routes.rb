ae5076661a4e9618d0d0942e922d4f439598df65
bruno gonzaga <babsgonzaga@gmail.com>
2021-11-12 12:57:18 -0300

    login modal pronto.

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,7 @@
 Rails.application.routes.draw do
-  # devise_for :users
-  devise_for :users, controllers: {
-        sessions: 'users/sessions'
-  }
+  devise_for :users, controllers: {sessions: 'users/sessions'}
   root to: 'pages#home'
-  get '/user' => "pages#minha", :as => :user_root
+  get '/user' => "pages#minha", as: :user_root
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :candidatos, only: [:index, :show] do
     resources :marcados, only: [:create]
