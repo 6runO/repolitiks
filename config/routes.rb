ff6b051a16fedde7292620851693b96fe25efe3c
bruno gonzaga <babsgonzaga@gmail.com>
2021-11-13 11:03:39 -0300

    Feito.

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
-  devise_for :users, controllers: {sessions: 'users/sessions'}
+  devise_for :users, controllers: {
+    sessions: 'users/sessions',
+    registrations: "users/registrations"
+  }
   root to: 'pages#home'
   get '/user' => "pages#minha", as: :user_root
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
