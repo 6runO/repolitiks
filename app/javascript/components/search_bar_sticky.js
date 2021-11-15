ad5982475a4a73abfa5d4f478b56df62a72d3ba0
bruno gonzaga <babsgonzaga@gmail.com>
2021-11-15 02:26:26 -0300

    Agora sim.

--- app/javascript/components/search_bar_sticky.js
+++ app/javascript/components/search_bar_sticky.js
@@ -1,21 +1,24 @@
 const searchBarSticky = () => {
   const searchBar = document.getElementById('search-bar-container');
   if (searchBar) {
-    // When the user scrolls the page, execute myFunction
-    window.onscroll = () => { myFunction() };
 
     // Get the offset position of the navbar
     const sticky = searchBar.offsetTop;
 
+    const bar = document.getElementById('search-bar');
+
     // Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
-    function myFunction() {
+    const myFunction = () => {
       if (window.pageYOffset >= sticky) {
-        searchBar.classList.remove("container")
+        bar.classList.add("w-100")
       } else {
-        navbar.classList.add("container");
+        bar.classList.remove("w-100");
       }
     }
+
+    // When the user scrolls the page, execute myFunction
+    window.onscroll = () => { myFunction() };
   }
 }
 
-  export { searchBarSticky };
+export { searchBarSticky };
