e1d10984f57f66a5c4b0c31aa9a89d1f47e6f4f5
bruno gonzaga <babsgonzaga@gmail.com>
2021-11-15 20:55:32 -0300

    Search modal mais.

--- app/javascript/components/search_bar_sticky.js
+++ app/javascript/components/search_bar_sticky.js
@@ -3,25 +3,35 @@ const searchBarSticky = () => {
   if (searchBarContainer) {
 
     // Get the offset position of the navbar
-    const sticky = searchBarContainer.offsetTop;
+    // const sticky = searchBarContainer.offsetTop;
 
     const searchBar = document.getElementById('search-bar');
 
-    // Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
-    const myFunction = () => {
-      if (window.pageYOffset >= sticky) {
+    window.addEventListener('scroll', () => {
+      if (window.scrollY >= window.innerHeight) {
         searchBarContainer.classList.add("sticky");
         searchBar.classList.add("w-100");
-        // searchBar.classList.add("bg-dark");
       } else {
         searchBarContainer.classList.remove("sticky");
         searchBar.classList.remove("w-100");
-        // searchBar.classList.remove("bg-dark");
       }
-    }
+    });
+
+    // Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
+    // const myFunction = () => {
+    //   if (window.pageYOffset >= sticky) {
+    //     searchBarContainer.classList.add("sticky");
+    //     searchBar.classList.add("w-100");
+    //     // searchBar.classList.add("bg-dark");
+    //   } else {
+    //     searchBarContainer.classList.remove("sticky");
+    //     searchBar.classList.remove("w-100");
+    //     // searchBar.classList.remove("bg-dark");
+    //   }
+    // }
 
     // When the user scrolls the page, execute myFunction
-    window.onscroll = () => { myFunction() };
+    // window.onscroll = () => { myFunction() };
   }
 }
 
--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -37,5 +37,5 @@ document.addEventListener('turbolinks:load', () => {
   // Call your functions here, e.g:
   // initSelect2();
   onSelectChange();
-  searchBarSticky();
+  // searchBarSticky();
 });
